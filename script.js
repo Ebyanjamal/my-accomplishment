0747bb53dfb59e1367bd694827832a967d595568
Ebyan Jamal <Ebyanjamal6@gmail.com>
2022-12-12 11:49:05 -0700

    commit tree ce0efb57eef7c5379bbb005929300b10f024800a

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const s = document.createElement("p");
+s.textContent = "Hello, World! The amount of time that I have written those two words in this pre-work is insane! My name is Ebyan Jamal I'm a Minnesota native that moved my way down to colorado about 2 years ago.A little something about myself is that computers have always fascinated me from my early ages. Whenever there would be a technical issue I would be the one to solve it. Just by following the computer therefore clicking a couple buttons and giving it the access it needs then voila! Not only is Software Development a subject that Im very interested to deeply explore, but when you code or debug successfully the feeling is compelling. Keeping me and many others wanting more!";
+document.querySelector("body").appendChild(s);
+const body = document.querySelector('body')
+const button= document.querySelector('button');
+button.addEventListener('click', function() {
+   body.style.backgroundColor = "white";
+  });
+
+  
+
